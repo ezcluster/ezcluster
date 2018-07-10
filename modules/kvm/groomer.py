30dbefa9b1d77cc8510ba9c6e50914d5fffdfd39
Serge ALEXANDRE <sa@broadsoftware.com>
2018-07-10 11:09:06 +0200

    Replacing 'pattern' by 'role'

--- modules/kvm/groomer.py
+++ modules/kvm/groomer.py
@@ -37,58 +37,58 @@ def groom(module, model):
         for path in model['cluster']['role_paths']:
             path = appendPath(model['data']['sourceFileDir'], path)
             model['data']["rolePaths"].add(path)
-    # ----------------------------------------- Handle patterns
-    model["data"]["patternByName"] = {}
-    if 'patterns' in model['cluster']:
-        for ptrn in model["cluster"]["patterns"]:
-            pattern = copy.deepcopy(ptrn)
-            model["data"]["patternByName"][pattern["name"]] = pattern
+    # ----------------------------------------- Handle roles
+    model["data"]["roleByName"] = {}
+    if 'roles' in model['cluster']:
+        for ptrn in model["cluster"]["roles"]:
+            role = copy.deepcopy(ptrn)
+            model["data"]["roleByName"][role["name"]] = role
             # -------------- Zone
-            zoneName = locate("zone", pattern, model["cluster"], "Pattern '{}': Missing zone definition (And no default value in cluster definition)".format(pattern["name"]))
+            zoneName = locate("zone", role, model["cluster"], "Role '{}': Missing zone definition (And no default value in cluster definition)".format(role["name"]))
             if zoneName not in model["infra"]["zoneByName"]:
-                ERROR("Pattern '{}': Unknow zone '{}'".format(pattern["name"], zoneName))
-            pattern["zone"] = zoneName
+                ERROR("Role '{}': Unknow zone '{}'".format(role["name"], zoneName))
+            role["zone"] = zoneName
             zone = model["infra"]["zoneByName"][zoneName]
-            pattern["network"] = model["infra"]["networkByName"][zone["network"]]
+            role["network"] = model["infra"]["networkByName"][zone["network"]]
             # ------------- domain
-            pattern['domain'] = locate("domain", pattern, model["cluster"], "Pattern '{}': Missing domain definition (And no default value in cluster definition)".format(pattern["name"]))
+            role['domain'] = locate("domain", role, model["cluster"], "Role '{}': Missing domain definition (And no default value in cluster definition)".format(role["name"]))
             # ------------- Template
-            tmplName = locate("kvm_template", pattern, model["cluster"], "Pattern '{}': Missing kvm_template definition (And no default value in cluster definition)".format(pattern["name"]))
+            tmplName = locate("kvm_template", role, model["cluster"], "Role '{}': Missing kvm_template definition (And no default value in cluster definition)".format(role["name"]))
             if tmplName not in model["infra"]["kvmTemplateByName"]:
-                ERROR("Pattern '{}': Unknow kvm_template '{}'".format(pattern["name"], tmplName))
-            pattern["kvmTemplate"] = model["infra"]["kvmTemplateByName"][tmplName]
+                ERROR("Role '{}': Unknow kvm_template '{}'".format(role["name"], tmplName))
+            role["kvmTemplate"] = model["infra"]["kvmTemplateByName"][tmplName]
             # Handle root disk logical volumes
-            if 'root_lvs' in pattern:
+            if 'root_lvs' in role:
                 vgSize = 0
                 modifiedLv = set()
-                for lv in pattern["root_lvs"]:
+                for lv in role["root_lvs"]:
                     modifiedLv.add(lv["name"])
-                    if "rootLvByName" not in pattern["kvmTemplate"] or lv["name"] not in pattern['kvmTemplate']['rootLvByName']:
-                        ERROR("Pattern '{}': Logical volume '{}' does not exists in the template".format(pattern["name"], lv["name"]))
+                    if "rootLvByName" not in role["kvmTemplate"] or lv["name"] not in role['kvmTemplate']['rootLvByName']:
+                        ERROR("Role '{}': Logical volume '{}' does not exists in the template".format(role["name"], lv["name"]))
                     else:
-                        templateLv = pattern['kvmTemplate']['rootLvByName'][lv["name"]]
+                        templateLv = role['kvmTemplate']['rootLvByName'][lv["name"]]
                         lv['fstype'] = templateLv["fstype"]
-                        lv["volgroup"] = pattern['kvmTemplate']['root_vg_name'] # Only one VG handled
+                        lv["volgroup"] = role['kvmTemplate']['root_vg_name'] # Only one VG handled
                         vgSize += lv["size"]
                         if lv["size"] < templateLv["size"]:
-                            ERROR("Pattern '{}': Logical volume '{}': size ({}GB) can't be lower than the one from template ({}GB) (Can't shrink LV)".format(pattern["name"], lv['name'], lv['size'], templateLv['size']))
+                            ERROR("Role '{}': Logical volume '{}': size ({}GB) can't be lower than the one from template ({}GB) (Can't shrink LV)".format(role["name"], lv['name'], lv['size'], templateLv['size']))
                 # To have accurate vg size, must add unmodified lv size
-                for tmplLvName, tmplLv in pattern['kvmTemplate']['rootLvByName'].iteritems():
+                for tmplLvName, tmplLv in role['kvmTemplate']['rootLvByName'].iteritems():
                     if tmplLvName not in modifiedLv:
                         vgSize += tmplLv['size'] 
-                if vgSize >= pattern['kvmTemplate']["root_vg_size"]:
-                    ERROR("Pattern '{}': Sum of all root LV must not exceed or equal template root PV ({} >= {})".format(pattern["name"], vgSize, pattern['kvmTemplate']["root_vg_size"]))
+                if vgSize >= role['kvmTemplate']["root_vg_size"]:
+                    ERROR("Role '{}': Sum of all root LV must not exceed or equal template root PV ({} >= {})".format(role["name"], vgSize, role['kvmTemplate']["root_vg_size"]))
             # ------------------------------------ Data disks
-            if "data_disks" in pattern:
-                for i in range(0, len(pattern['data_disks'])):
-                    pattern['data_disks'][i]['device'] = model['infra']['deviceFromIndex'][i]
+            if "data_disks" in role:
+                for i in range(0, len(role['data_disks'])):
+                    role['data_disks'][i]['device'] = model['infra']['deviceFromIndex'][i]
                 disksToMount = 0
-                for d in pattern['data_disks']:
+                for d in role['data_disks']:
                     if "mount" in d:
                         disksToMount += 1
-                pattern["disksToMountCount"] = disksToMount
+                role["disksToMountCount"] = disksToMount
             else:
-                pattern["disksToMountCount"] = 0
+                role["disksToMountCount"] = 0
     # ----------------------------------------- Handle nodes
     if 'nodes' in model['cluster']:
         nodeByIp = {}  # Just to check duplicated ip
@@ -99,10 +99,10 @@ def groom(module, model):
                 node['hostname'] = node['name']
             if "vmname" not in node:
                 node['vmname'] = model['cluster']['id'] + "_" + node['name']
-            if node['pattern'] not in model['data']['patternByName']:
-                ERROR("Node '{}' reference an unexisting pattern ({})".format(node["name"], node['pattern']))
-            pattern =  model['data']['patternByName'][node['pattern']]
-            node["fqdn"] = node['hostname'] + "." + pattern['domain']
+            if node['role'] not in model['data']['roleByName']:
+                ERROR("Node '{}' reference an unexisting role ({})".format(node["name"], node['role']))
+            role =  model['data']['roleByName'][node['role']]
+            node["fqdn"] = node['hostname'] + "." + role['domain']
             ip = node['ip'] = resolveDns(node['fqdn'])
             if ip == None:
                 ERROR("Unable to lookup an IP for node '{0}' ({1})'.".format(node['name'], node['fqdn']))
@@ -110,7 +110,7 @@ def groom(module, model):
                 nodeByIp[ip] = node
             else:
                 ERROR("Same IP ({}) used for both node '{}' and '{}'".format(ip, nodeByIp[ip]['name'], node['name']))
-            network =  pattern["network"]
+            network =  role["network"]
             node['network'] = network['name']
             if ipaddress.ip_address(u"" + ip) not in network['cidr']:
                 ERROR("IP '{}' not in network '{}' for node {}".format(ip, network['name'], node['name']))
@@ -124,22 +124,22 @@ def groom(module, model):
             nbrRootVolumes = len(host['root_volumes'])
             node['rootVolume'] = host['root_volumes'][idx % nbrRootVolumes]['path']
             # Handle data disk
-            if 'data_disks' in pattern and len(pattern["data_disks"]) > 0:
-                dataDisks = copy.deepcopy(pattern['data_disks'])
+            if 'data_disks' in role and len(role["data_disks"]) > 0:
+                dataDisks = copy.deepcopy(role['data_disks'])
                 nbrDataVolume = len(model['infra']['hostByName'][node['host']]['data_volumes'])
                 if "data_volume_index" in node:
                     for i in range(len(dataDisks)):
                         dataDisks[i]['volume'] = model['infra']['hostByName'][node['host']]['data_volumes'][(i + node['data_volume_index']) % nbrDataVolume]['path']
                 elif "data_volume_indexes" in node:
                     if len(node['data_volume_indexes']) != len(dataDisks):
-                        ERROR("Node {0}: data_volume_indexes size ({1} != pattern.data_disks size ({2})".format(node['name'], node['data_volume_indexes'], len(dataDisks)))
+                        ERROR("Node {0}: data_volume_indexes size ({1} != role.data_disks size ({2})".format(node['name'], node['data_volume_indexes'], len(dataDisks)))
                     for i in range(len(dataDisks)):
                         dataDisks[i]['volume'] = model['infra']['hostByName'][node['host']]['data_volumes'][node['data_volume_indexes'][i] % nbrDataVolume]['path']
                 else:
                     ERROR("Node {0}: Either 'data_volume_index' or 'data_volume_indexes' must be defined!".format(node['name']))
                 dataDisksByNode[node["name"]] = dataDisks
-            memoryByHost[host['name']]['sum'] += pattern['memory']
-            memoryByHost[host['name']]['detail'][node['name']] = pattern['memory']
+            memoryByHost[host['name']]['sum'] += role['memory']
+            memoryByHost[host['name']]['detail'][node['name']] = role['memory']
         
     print "------------- Memory usage per host"
     l = list( model['data']['memoryByHost'].keys())
