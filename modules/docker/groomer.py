ca881dc8d2315b7f797a9c4183d255856318bdb0
Serge ALEXANDRE <sa@broadsoftware.com>
2018-08-02 20:01:45 +0200

    Added docker module

---
+++ modules/docker/groomer.py
@@ -0,0 +1,9 @@
+
+
+
+
+def groom(module, model):
+    if not "docker" in model["cluster"]:
+        model["cluster"]["docker"] = {}
+    if not "users" in model["cluster"]["docker"]:
+        model["cluster"]["docker"]["users"] = []
