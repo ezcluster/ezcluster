fa05375d78e1dd1111349ffc9d964b4b02d2fe35
Serge ALEXANDRE <sa@broadsoftware.com>
2018-09-16 19:48:51 +0200

    refactor elasticsearch module to allow several node per VM.

--- modules/elastic/groomer.py
+++ modules/elastic/groomer.py
@@ -7,48 +7,70 @@ from schema import schemaMerge
 
 
 CLUSTER="cluster"
-NODES="nodes"
 ELASTICSEARCH="elasticsearch"
 ROLE="role"
 DATA="data"
-ROLE_BY_NAME="roleByName"
-_ELASTICSEARCH_="_elasticsearch_"
 NAME="name"
+ES_NODES="es_nodes"
+ROLES="roles"
+ESNODES="esNodes"
+ES_CONFIG="es_config"
+NODE_MASTER="node.master"
+VARS="vars"
+ES_INSTANCE_NAME="es_instance_name"
 
 def groom(module, model):
+    
+    f = os.path.join(module.path, "default.yml")
+    if os.path.exists(f):
+        base = yaml.load(open(f))
+    else:
+        base = {}
+        
+    model[DATA][ESNODES] = []        
     """ 
-    Will merge elasticsearch vars from:
+    For each es_node, will merge elasticsearch vars from:
     - Module default configuration file
     - global from cluster definition file
-    - group
-    - node  """
-    for node in model[CLUSTER][NODES]:
-        f = os.path.join(module.path, "default.yml")
-        if os.path.exists(f):
-            map = yaml.load(open(f))
-        else:
-            map = {}
-        # Add global value
-        if ELASTICSEARCH in model[CLUSTER]:
-            if not isinstance(model[CLUSTER][ELASTICSEARCH], dict):
-                ERROR("Invalid global '{}' definition:  not a dictionary".format(ELASTICSEARCH))
-            else:
-                map = schemaMerge(map, model[CLUSTER][ELASTICSEARCH])
-        # Add the role specific value
-        if ROLE in node:
-            role = model[DATA][ROLE_BY_NAME][node[ROLE]]
-            if ELASTICSEARCH in role:
-                if not isinstance(role[ELASTICSEARCH], dict):
-                    ERROR("Invalid role definition ('{}'):  '{}' is not a dictionary".format(role[NAME], ELASTICSEARCH))
+    - parent role
+    - es_node """
+    for role in model[CLUSTER][ROLES]:
+        if ES_NODES in role:
+            index = -1
+            for esnode in role[ES_NODES]:
+                index += 1
+                map = copy.deepcopy(base)
+                # Add global value
+                if ELASTICSEARCH in model[CLUSTER]:
+                    if not isinstance(model[CLUSTER][ELASTICSEARCH], dict):
+                        ERROR("Invalid global '{}' definition:  not a dictionary".format(ELASTICSEARCH))
+                    else:
+                        map = schemaMerge(map, model[CLUSTER][ELASTICSEARCH])
+                # Add the role specific value
+                if ELASTICSEARCH in role:
+                    if not isinstance(role[ELASTICSEARCH], dict):
+                        ERROR("Invalid role definition ('{}'):  '{}' is not a dictionary".format(role[NAME], ELASTICSEARCH))
+                    else:
+                        map = schemaMerge(map, role[ELASTICSEARCH])
+                # And get the es_node specific value
+                if not isinstance(esnode, dict):
+                    ERROR("Invalid es_node definition in role '{}':  item#{} is not a dictionary".format(role[NAME], index))
                 else:
-                    map = schemaMerge(map, role[ELASTICSEARCH])
-        # And get the node specific value
-        if ELASTICSEARCH in node:
-            if not isinstance(node[ELASTICSEARCH], dict):
-                ERROR("Invalid node definition ('{}'):  '{}' is not a dictionary".format(node[NAME], ELASTICSEARCH))
-            else:
-                map = schemaMerge(map, node[ELASTICSEARCH])
-        node[_ELASTICSEARCH_] = map
-        
+                    map = schemaMerge(map, esnode)
+                if not ES_CONFIG in map or not NODE_MASTER in map[ES_CONFIG]:
+                    ERROR("Invalid es_node definition in role '{}, item#{}: es_config.'node.master' must be defined".format(role[NAME], index))
+                if not ES_INSTANCE_NAME in map:
+                    ERROR("Invalid es_node definition in role '{}, item#{}: es_instance_name must be defined".format(role[NAME], index))
+                esn = {}
+                esn[ROLE] = role[NAME]
+                esn[VARS] = map
+                model[DATA][ESNODES].append(esn)
+    # We must arrange for master nodes to be deployed first.
+    model[DATA][ESNODES].sort(key=keyFromEsNode, reverse=False)        
+    
+
+
+def keyFromEsNode(esNode):
+    return "0" if esNode[VARS][ES_CONFIG][NODE_MASTER] else "1"       
         
             
