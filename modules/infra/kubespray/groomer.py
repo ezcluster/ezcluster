d881e4a01285c63e6dd179319e998cca2b669676
Serge ALEXANDRE <sa@broadsoftware.com>
2018-10-16 00:10:42 +0200

    Work in progress on kubespray module

--- modules/infra/kubespray/groomer.py
+++ modules/infra/kubespray/groomer.py
@@ -6,3 +6,5 @@ def groom(module, model):
     repo_folder = appendPath(model["data"]["sourceFileDir"],  model["cluster"]["kubespray"]["repo_folder"]) 
     model["cluster"]["kubespray"]["repo_folder"] = repo_folder
     model["data"]["rolePaths"].add(appendPath(repo_folder, "roles"))
+    
+    model["data"]["dnsNbrDots"] = model["cluster"]["domain"].count(".") + 1
